ff4dd32d8d131401ba3a6ec2700ec4171be2fb93
gallifreych <tubaekmen0@gmail.com>
2025-07-09 13:06:10 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,111 @@
+import tkinter
+import random
+import time
+
+ROWS = 25
+COLS = 25
+KARE = 25
+
+WINDOW_WIDTH = KARE * ROWS
+WINDOW_HEIGHT = KARE * COLS
+
+class Tile:
+    def __init__(self, x, y):
+        self.x =x
+        self.y =y
+
+window = tkinter.Tk()
+window.title("Yılan Oyunu")
+window.resizable(False,False)
+
+canvas = tkinter.Canvas(window, bg= "DarkGreen", width= WINDOW_WIDTH, height=WINDOW_HEIGHT, borderwidth=0, highlightthickness=0)
+
+canvas.pack()
+window.update()
+
+PENCERE_WIDTH = window.winfo_width()
+PENCERE_HEIGHT = window.winfo_height()
+EKRAN_WIDTH = window.winfo_screenwidth()
+EKRAN_HEIGHT = window.winfo_screenheight()
+
+window_x = int((EKRAN_WIDTH/2) - (PENCERE_WIDTH/2))
+window_y = int((EKRAN_HEIGHT/2) - (PENCERE_HEIGHT/2))
+
+#pencereyi ekranın ortasında aç
+#formül= width x height + x + y.
+window.geometry(f"{PENCERE_WIDTH}x{PENCERE_HEIGHT}+{window_x}+{window_y}")
+
+#yılanı ve yemeği şu karede başlat
+snake= Tile(10*KARE,10*KARE) 
+food = Tile(15*KARE,10*KARE)
+snake_body = []
+velocityX = 0
+velocityY = 0
+gameOver = False
+def change_direction(e):
+   global velocityX, velocityY, gameOver
+   if(e.keysym== "Up" and velocityY !=1):
+    velocityX= 0
+    velocityY= -1
+   elif(e.keysym == "Down" and velocityY != -1):
+    velocityX = 0
+    velocityY = 1
+   elif(e.keysym == "Left" and velocityX != 1):
+    velocityX = -1
+    velocityY = 0
+   elif(e.keysym == "Right" and velocityX != -1):
+    velocityX = 1
+    velocityY = 0
+    if(gameOver):
+       return
+    
+def move():
+    global snake ,food, snake_body, gameOver
+    if(gameOver):
+       return
+    #kenarlara veya kendine çarparsa game over
+    if(snake.x < 0 or snake.x >= WINDOW_WIDTH or snake.y < 0 or snake.y >=WINDOW_HEIGHT):
+           gameOver = True
+           return
+    
+    for tile in snake_body:
+       if(snake.x == tile.x and snake.y == tile.y):
+          gameOver = True
+          return
+
+    if (snake.x == food.x and snake.y == food.y):
+        snake_body.append(Tile(food.x, food.y))
+        food.x = random.randint(0,COLS-1) * KARE
+        food.y = random.randint(0,ROWS-1) * KARE
+
+    for i in range(len(snake_body)-1, -1, -1):
+       tile = snake_body[i]
+       if(i == 0):
+           tile.x = snake.x
+           tile.y = snake.y
+       else:
+           prev_tile = snake_body[i-1]
+           tile.x = prev_tile.x
+           tile.y = prev_tile.y
+
+        
+    snake.x += velocityX * KARE
+    snake.y += velocityY * KARE
+
+def draw():
+    global snake
+    move()
+    canvas.delete("all")
+    #yem
+    canvas.create_rectangle(food.x, food.y, food.x+KARE, food.y+KARE, fill="red")
+    #yılan
+    canvas.create_rectangle(snake.x, snake.y, snake.x+KARE, snake.y+KARE, fill="white")
+
+    for tile in snake_body:
+       canvas.create_rectangle(tile.x, tile.y, tile.x+KARE, tile.y+KARE, fill="white")
+    #0.1 saniye sonra tekrar çiz (sürekli çizer)
+    window.after(100,draw) 
+draw()
+
+window.bind("<KeyRelease>", change_direction)
+window.mainloop()
